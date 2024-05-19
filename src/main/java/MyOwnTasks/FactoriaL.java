dec0e06a752b074bca1c4a3ffc84ec12de0843d4
Bakulya <blarsenova@gmail.com>
2024-05-19 15:22:42 -0500

    added fib classes with 2 methods

--- src/main/java/MyOwnTasks/FactoriaL.java
+++ src/main/java/MyOwnTasks/FactoriaL.java
@@ -1,4 +1,4 @@
-package org.example;
+package MyOwnTasks;
 
 public class FactoriaL {
 
---
+++ src/main/java/MyOwnTasks/Fibonacci.java
@@ -0,0 +1,48 @@
+package MyOwnTasks;
+
+public class Fibonacci {
+ // using Recursion method
+    static int n1=0,n2=1,n3=0;
+    static void printFibonacci(int count){
+        if(count>0){
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+            System.out.print(" "+n3);
+            printFibonacci(count-1);
+        }
+    }
+    public static void main(String args[]){
+        int count=14;
+        System.out.print(n1+" "+n2);//printing 0 and 1
+        printFibonacci(count-2);//n-2 because 2 numbers are already printed
+    }
+
+
+    //using iterative approach
+
+    static void Fibonacci(int N)
+    {
+        int num1 = 0, num2 = 1;
+
+        for (int i = 0; i < N; i++) {
+            // Print the number
+            System.out.print(num1 + " ");
+
+            // Swap
+            int num3 = num2 + num1;
+            num1 = num2;
+            num2 = num3;
+        }
+    }
+
+        // Given Number N
+        int N = 10;
+    //not completed here yet
+
+    }
+
+
+
+
+
