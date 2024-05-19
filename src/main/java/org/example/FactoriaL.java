fe3dba2c25efd1f4af759ec93794ee0be5440c8f
Bakulya <blarsenova@gmail.com>
2024-05-19 14:45:00 -0500

    factorial using java

---
+++ src/main/java/org/example/FactoriaL.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class FactoriaL {
+
+    public static void main(String args[]){
+        int i,fact=1;
+        int number=8;//It is the number to calculate factorial
+        for(i=1;i<=number;i++){
+            fact=fact*i;
+        }
+        System.out.println("Factorial of "+number+" is: "+fact);
+    }
+
+}
