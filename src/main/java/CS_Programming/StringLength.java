e553064c4e374518ba87dc34b0bacaf01e761d7e
Bakulya <blarsenova@gmail.com>
2024-05-23 18:29:16 -0500

    String length class

---
+++ src/main/java/CS_Programming/StringLength.java
@@ -0,0 +1,15 @@
+package CS_Programming;
+
+public class StringLength {
+
+    public static void main(String args[]){
+
+        String word = "Summer is hot";
+        System.out.println("The length of the given word without white spaces is: "+ word.replace(" ", "").length());
+
+
+    }
+
+
+
+}
