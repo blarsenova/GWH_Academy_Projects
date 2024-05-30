fac3364381c08c5fbc75097cb82faedc61f47153
Bakulya <blarsenova@gmail.com>
2024-05-29 23:36:42 -0500

    check if array is sorted in ascending order, added main method

--- src/main/java/CS_Programming/SortedArray.java
+++ src/main/java/CS_Programming/SortedArray.java
@@ -2,24 +2,31 @@ package CS_Programming;
 
 public class SortedArray {
 
- public static boolean isSortedArray (int arr[]{
+    public static boolean isSortedArray(int arraySorted[]) {
 
-         int [] arraySorted = new int[] {4,6,1,14,55,97};
-
-
-         //check below if array is sorted in ascending order
-         for (int i=0; i<arraySorted.length-1; i++) {
+        //check below if array is sorted in ascending order
+        for (int i = 0; i < arraySorted.length - 1; i++) {
             if (arraySorted[i] > arraySorted[i + 1]) {
                 return false;
 
-            } else {
-                return true;
             }
         }
-        public static void main (String args []){
-
-
-        }
-
+        return true;
     }
-}
+
+    public static void main(String args[]) {
+        int[] arraySorted = {4, 6, 10, 14, 55, 97};
+        boolean isSorted = isSortedArray(arraySorted);
+
+        if (isSorted) {
+            System.out.println("The array is sorted in ascending order.");
+        } else {
+            System.out.println("The array is not sorted in ascending order");
+
+    } }}
+
+
+
+
+
+
