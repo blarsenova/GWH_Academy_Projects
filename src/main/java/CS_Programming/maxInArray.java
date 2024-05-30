f5844674117293c2a108cdd8fac0a00f591630d8
Bakulya <blarsenova@gmail.com>
2024-05-29 23:40:07 -0500

    find a max in array

---
+++ src/main/java/CS_Programming/maxInArray.java
@@ -0,0 +1,14 @@
+package CS_Programming;
+
+public class maxInArray {
+    public static void main (String args[]) {
+
+        int maxArray[]= {300, 6, 876};
+        int max;
+
+        for (int i=0;i<maxArray.length-1;i++){
+
+
+        }
+    }
+}
