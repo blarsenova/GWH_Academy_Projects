c8d65b7ec394206b62510712de34d2d29909ab75
Bakulya <blarsenova@gmail.com>
2024-06-13 23:46:20 -0500

    fib with recursion and memoization

--- src/main/java/MyOwnTasks/FibonacciWithRecursionMemo.java
+++ src/main/java/MyOwnTasks/FibonacciWithRecursionMemo.java
@@ -2,25 +2,23 @@ package MyOwnTasks;
 import java.util.HashMap;
 import java.util.Map;
 public class FibonacciWithRecursionMemo {
-    private Map<Integer, Integer> memo = new HashMap<>();
-
+    private static long[] fibonacciCache;
     // Method to calculate Fibonacci number using recursion with memoization
-    public int fibonacci(int n) {
-        if (n <= 0) {
-            throw new IllegalArgumentException("n must be a positive integer");
+    private static long fibonacci(int n) {
+        if (n <= 1) {
+            return n;
         }
-        if (n == 1) {
-            return 0; // The first Fibonacci number is 0
+        if (fibonacciCache[n] != 0) {
+            return fibonacciCache[n];
         }
-        if (n == 2) {
-            return 1; // The second Fibonacci number is 1
-        }
-        if (memo.containsKey(n)) {
-            return memo.get(n); // Return the cached value if it exists
-        }
-        int result = fibonacci(n - 1) + fibonacci(n - 2);
-        memo.put(n, result); // Cache the computed result
-        return result;
+        long nthFibNumber = (fibonacci(n - 1) + fibonacci(n - 2));
+        fibonacciCache[n] = nthFibNumber;
+        return nthFibNumber;
     }
-
+    public static void main(String[] args){
+            int n = 100;
+            fibonacciCache = new long[n + 1];
+            System.out.print(fibonacci(n));
+    }
 }
+
