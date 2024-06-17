c6879e0a174f63a42716e1f98d40f282d8c9155a
Bakulya <blarsenova@gmail.com>
2024-06-16 19:32:18 -0500

    added continuation for p1 classes

--- src/main/java/CS_Programming/CompressionInfoFolder.java
+++ src/main/java/CS_Programming/CompressionInfoFolder.java
@@ -1,14 +1,29 @@
 package CS_Programming;
 
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
-public class CompressionInfoFolder {
+public class CompressionInfoFolder implements Serializable {
+
+    private final Map<Short, String> codeToWords1;
+    private final byte[] codedText;
 
     Map<Short, String> codeMap;
     short[] codeToWords;
 
 
+    public CompressionInfoFolder(Map<Short, String> codeToWords, byte[] codedText) {
 
+        codeToWords1 = codeToWords;
+        this.codedText=codedText;
+    }
 
+    public Map<Short, String> getCodeToWords1() {
+        return codeToWords1;
+    }
+
+    public byte[] getCodedText() {
+        return codedText;
+    }
 }
--- src/main/java/CS_Programming/P1_with_Taalai.java
+++ src/main/java/CS_Programming/P1_with_Taalai.java
@@ -6,40 +6,55 @@ import java.util.Map;
 
 public class P1_with_Taalai {
     public static void main(String[] args) throws CompressionException {
+
         String inputFile = "/Users/baktygullarsenova/GWH Academy Projects/inputp1.txt";
-        String outputFile = inputFile+ ".sc";
+        String outputFile = inputFile + ".sc";
 
         try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
-            ObjectOutputStream writer= new ObjectOutputStream(new FileOutputStream(outputFile));
-          ByteArrayOutputStream codeToText =  new ByteArrayOutputStream();
-            Map<String, Short> wordToCode =new HashMap<>();
+          //  ObjectOutputStream writer = new ObjectOutputStream(new FileOutputStream(outputFile));
+            ByteArrayOutputStream codedText = new ByteArrayOutputStream();
+
+            Map<String, Short> wordToCode = new HashMap<>();
             Map<Short, String> codeToWords = new HashMap<>();
 
-            short codeCounter =0;
+            short codeCounter = 0;
             String line;
+
+
             while ((line = reader.readLine()) != null) {
                 String[] wordsInFile = line.split("\\s+");
-                for (String w:wordsInFile){
-                   Short existingCode = wordToCode.get(w);
-                   if (existingCode==null){
-                       wordToCode.put(w, codeCounter);
-                       codeToWords.put(codeCounter,w);
-                       codeCounter++;
-                       if (codeCounter==Short.MAX_VALUE){
-                           throw new CompressionException();
-                       }
-                   }
-                    System.out.println(w);
+                for (String w : wordsInFile) {
+                    Short existingCode = wordToCode.get(w);
+
+                    if (existingCode == null) {
+                        wordToCode.put(w, codeCounter);
+                        codeToWords.put(codeCounter, w);
+
+                        byte high = (byte)(codeCounter>>>8);
+                        byte low = (byte)codeCounter;
+                        codedText.write(high);
+                        codedText.write(low);
+
+                        codeCounter++;
+
+                        if (codeCounter == Short.MAX_VALUE) {
+                            throw new CompressionException();
+                        }
+                    }
                 }
-                System.out.println();
-                line=reader.readLine();
+                line = reader.readLine();
             }
-            System.out.println("Number of words found: "+codeCounter);
+    ObjectOutputStream writer = new ObjectOutputStream(new FileOutputStream(outputFile));
+        CompressionInfoFolder holder = new CompressionInfoFolder(codeToWords, codedText.toByteArray());
+        writer.writeObject(holder);
+        writer.flush();
+            System.out.println("Number of words found: " + codeCounter);
 
         } catch (FileNotFoundException e) {
             System.out.println("Sorry, the file is not found to read: " + inputFile);
         } catch (IOException e) {
             System.out.println("Sorry, error encountered while reading a file: " + inputFile);
+            e.printStackTrace();
         }
     }
 }
