0359c08b0c654100d193d7479d96fa660bbe1f60
Bakulya <blarsenova@gmail.com>
2024-06-16 20:08:56 -0500

    added continuation for p1 classes

--- src/main/java/CS_Programming/CompressionException.java
+++ src/main/java/CS_Programming/CompressionException.java
@@ -1,4 +1,17 @@
 package CS_Programming;
 
-public class CompressionException extends Throwable {
+public class CompressionException extends Exception {
+
+    public CompressionException (String s){
+        super(s);
+
+    }
+
+    public  CompressionException(Exception e){
+        super(e);
+    }
+
+    public CompressionException() {
+        super();
+    }
 }
--- src/main/java/CS_Programming/P1_with_Taalai.java
+++ src/main/java/CS_Programming/P1_with_Taalai.java
@@ -8,10 +8,11 @@ public class P1_with_Taalai {
     public static void main(String[] args) throws CompressionException {
 
         String inputFile = "/Users/baktygullarsenova/GWH Academy Projects/inputp1.txt";
-        String outputFile = inputFile + ".sc";
+        String outputFile = inputFile + ".sc"; //decompressed file
+        String decompressedFile = outputFile + ".txt";
 
         try (BufferedReader reader = new BufferedReader(new FileReader(inputFile))) {
-          //  ObjectOutputStream writer = new ObjectOutputStream(new FileOutputStream(outputFile));
+            //  ObjectOutputStream writer = new ObjectOutputStream(new FileOutputStream(outputFile));
             ByteArrayOutputStream codedText = new ByteArrayOutputStream();
 
             Map<String, Short> wordToCode = new HashMap<>();
@@ -30,8 +31,8 @@ public class P1_with_Taalai {
                         wordToCode.put(w, codeCounter);
                         codeToWords.put(codeCounter, w);
 
-                        byte high = (byte)(codeCounter>>>8);
-                        byte low = (byte)codeCounter;
+                        byte high = (byte) (codeCounter >>> 8);
+                        byte low = (byte) codeCounter;
                         codedText.write(high);
                         codedText.write(low);
 
@@ -44,17 +45,36 @@ public class P1_with_Taalai {
                 }
                 line = reader.readLine();
             }
-    ObjectOutputStream writer = new ObjectOutputStream(new FileOutputStream(outputFile));
-        CompressionInfoFolder holder = new CompressionInfoFolder(codeToWords, codedText.toByteArray());
-        writer.writeObject(holder);
-        writer.flush();
+            ObjectOutputStream writer = new ObjectOutputStream(new FileOutputStream(outputFile));
+            CompressionInfoFolder holder = new CompressionInfoFolder(codeToWords, codedText.toByteArray());
+            writer.writeObject(holder);
+            writer.flush();
+            writer.close();
             System.out.println("Number of words found: " + codeCounter);
+
+            ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(decompressedFile));
+            Object inputObject = inputStream.readObject();
+            if (inputObject instanceof CompressionInfoFolder) {
+                throw new CompressionException("Unexpected type received. Program expects CompressionInfoHolder class " + CompressionInfoFolder.class.getCanonicalName());
+            }
+            CompressionInfoFolder inputHolder = (CompressionInfoFolder) inputObject;
+            BufferedWriter decompressingWriter = new BufferedWriter(new FileWriter(decompressedFile));
+            byte [] codedBytes = inputHolder.getCodedText();
+            for (int i=0; i<codedBytes.length;i+=2){
+                short high = codedBytes[i];
+                short low = codedBytes[i+1];
+                short code= (short)((high<<8)+low);
+
+            }
+            //Stopped here at 1:23 min
 
         } catch (FileNotFoundException e) {
             System.out.println("Sorry, the file is not found to read: " + inputFile);
         } catch (IOException e) {
             System.out.println("Sorry, error encountered while reading a file: " + inputFile);
             e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
 }
